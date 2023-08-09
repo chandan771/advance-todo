6d20aba8abccc198fb3f888783e1f56815039fb9
Chandan Kumar <111229538+chandan771@users.noreply.github.com>
2023-08-09 04:37:50 +0000

    ad-1

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client';
+// import { TodosProvider } from './store/Todos';
+
+
+// ReactDOM.createRoot(document.getElementById('root'))
